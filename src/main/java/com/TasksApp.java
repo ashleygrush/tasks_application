d030813a474e9b7e9d6d6661304988fc45925dd0
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-08-03 13:00:39 -0700

    server connection modifications.

--- src/main/java/com/TasksApp.java
+++ src/main/java/com/TasksApp.java
@@ -2,6 +2,8 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,10 +13,20 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 
 @EnableCaching
-public class TasksApp {
+public class TasksApp extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TasksApp.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TasksApp.class);
+
+    }
 }
+
+// ************* TO RUN LOCALLY ******************
+
+// to run locally again,
+// comment out database location in application.properties; added "provided" dependency in pom file and  extends SpringBootServleetInit. and Override above.
